8eac85c3d745794aac63c1f8a6d727b2d4269865
borvil <31356001+borvil@users.noreply.github.com>
2018-11-17 20:01:22 +0100

    Add files via upload

---
+++ boris_spring_boot_microservices_series/catalog-service/src/main/java/homework/catalogservice/Product.java
@@ -0,0 +1,18 @@
+package homework.catalogservice;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String code;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private double price;
+}
---
+++ boris_spring_boot_microservices_series/catalog-service/src/main/java/homework/catalogservice/ProductService.java
@@ -0,0 +1,28 @@
+package homework.catalogservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findProductByCode(String code) {
+        return productRepository.findByCode(code);
+    }
+}
